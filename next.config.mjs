3c08873933bb1728ce6897e4ddf85ba0c2781ea5
Luipoleon <luis.luigis.9999@gmail.com>
2024-05-04 10:58:44 -0600

    Proyect configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+
 const nextConfig = {};
+
+nextConfig.sassOptions = {
+  // Configure the route for sass files
+  "includePaths": [path.join(new URL(import.meta.url).pathname, "public/styles")],
+};
 
 export default nextConfig;
