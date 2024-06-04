cf55d39c66de6717bfc05fd1a24aa88c3cc99ee8
Luipoleon <luis.luigis.9999@gmail.com>
2024-06-03 18:57:58 -0600

    Nav bar / Change background and add message

--- src/app/components/MainContent.js
+++ src/app/components/MainContent.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import TypingEffect  from "./TypingEffect";
 const MainContent = () => {
 
     return (
@@ -8,6 +8,11 @@ const MainContent = () => {
                <div className="hero_image__background">
                </div>
                <div className="hero_image__background_content">
+                    <div className="hero_image__content">
+                        <div className="hero_image__content__title">
+                            <h1><TypingEffect typingData={["<code>Crafting Digital Landscapes</code>"]}/></h1>
+                        </div>
+                    </div>
                </div>
             </div>
 
--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,6 +1,17 @@
+import { FaBars } from "react-icons/fa";
+
 const NavBar = () =>{
-    return (<div className="navbar">
-       
+    return (
+    <div className="navbar">
+       <div className="navbar__button"><FaBars/></div>
+       <div className="navbar__languages"> 
+              <div className='language'>
+                EN
+              </div>
+              <div className='language selected'>
+                ES
+              </div>
+       </div>
     </div>);
 }
 
